fccb61e6dc0d541b5b819ead087f0099ac58bb42
Allahverdi <allahverdikrimov77@gmail.com>
2025-10-27 19:01:09 +0400

    Add files via upload

---
+++ task3 level3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int i=1;
+    int a=0;
+    while (i<100){
+        a=a+i;
+        i++;
+        
+    }
+    cout<<a;
+    return 0;
+}
