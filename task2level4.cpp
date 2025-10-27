c035e707f28d8cccd6098ef1443a9789ed181bb0
Allahverdi <allahverdikrimov77@gmail.com>
2025-10-27 21:26:22 +0400

    Add files via upload

---
+++ task2level4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,
+    count=0;
+    cin>>n;
+    if(n==0) count=1;
+    while(n!=0){
+        n/=10;
+        count++;
+    }
+    cout<<count;
+}
+
+  
